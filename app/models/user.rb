70835d7963fd7493ca38541cc13d7e738f606ff8
harupis <miiraandlee39391@gmail.com>
2020-05-22 06:41:14 +0000

    follow, unfollow

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,29 @@
 class User < ApplicationRecord
-    before_save { self.email.downcase! }
-    validates :name, presence: true, length: { maximum: 50 }
-    validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
-                    uniqueness: { case_sensitive: false }
-    has_secure_password
-    
-    has_many :microposts
+  before_save { self.email.downcase! }
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 },
+                  format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                  uniqueness: { case_sensitive: false }
+  has_secure_password
+  
+  has_many :microposts
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :followers, through: :reverses_of_relationship, source: :user
+  
+  def follow(other_user)
+    unless self == other_user
+      self.relationships.find_or_create_by(follow_id: other_user.id)
+    end
+  end
+
+  def unfollow(other_user)
+    relationship = self.relationships.find_by(follow_id: other_user.id)
+    relationship.destroy if relationship
+  end
+
+  def following?(other_user)
+    self.followings.include?(other_user)
+  end
 end
