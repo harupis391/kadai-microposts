22ee9fe62ab712dcab8f46cb4d7887ea105ac7bc
harupis <miiraandlee39391@gmail.com>
2020-05-23 07:54:00 +0000

    timeline

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -2,7 +2,7 @@ class ToppagesController < ApplicationController
   def index
     if logged_in?
       @micropost = current_user.microposts.build  # form_with 用
-      @microposts = current_user.microposts.order(id: :desc).page(params[:page])
+      @microposts = current_user.feed_microposts.order(id: :desc).page(params[:page])
     end
   end
 end
